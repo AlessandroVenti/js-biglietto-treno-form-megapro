379574294ffaf1ae64cc5dbe36ca8451a1dd2909
Alessandro Venti <alessandro.venti@icloud.com>
2021-03-11 14:04:59 +0100

    Js done 100%, css done 100%, html done 100%

--- js/script.js
+++ js/script.js
@@ -9,26 +9,29 @@ generaBtn.addEventListener('click', function() {
 
     var name = document.getElementById("name").value;
     var distanza = document.getElementById("distanza").value;
-    var età = document.getElementById("età").value;
-    console.log(name, distanza, età);
+    var eta = document.getElementById("età").value;
+    var randomCp = Math.round(Math.random() * (100000 - 90000)+ 90000);
+    var randomCarriage = Math.round(Math.random() * (20 - 1) + 1);
+    console.log(name, distanza, eta);
 
     document.getElementById("thename").innerHTML = name;
 
-    if(name !== "" && distanza > 0 && età !== "") {
+    if(name !== "" && distanza > 0 && eta !== "") {
         // console.log("ok");
         prezzoTotale = prezzoCorrente * distanza;
         console.log(prezzoTotale);
         
         //Blocco else-if per applicazione di sconto
 
-        if (età === 1) {
+        if (eta == 0) {
             // console.log("minorenne", età);
             var sconto = prezzoTotale * 0.2;
             prezzoTotale -= sconto;
+            document.getElementById("offerta").innerHTML = "sconto-minorenne";
 
             
-        } else if (età === 3) {
-            console.log("over65", età);
+        } else if (eta == 2) {
+            console.log("over65", eta);
             var sconto = prezzoTotale * 0.4;
             prezzoTotale -= sconto;
         }
@@ -36,10 +39,11 @@ generaBtn.addEventListener('click', function() {
 
     //fine blocco else-if per applicazione sconto
 
-    document.getElementById("costo-totale").innerHTML = prezzoTotale;
+    document.getElementById("costo-totale").innerHTML = prezzoTotale.toFixed(2) + "€";
 
-    var randomCp = Math.round(Math.random() * (100000 - 90000)+ 90000);
     document.getElementById("cp").innerHTML = randomCp;
+
+    document.getElementById("carriage").innerHTML = randomCarriage;
 
 });
 
